268b6bcec7da7726c90c854c82babddc648399b5
xmoure <ximenamoure@gmail.com>
2025-02-27 17:17:43 -0300

    changed index

--- index.js
+++ index.js
@@ -7,9 +7,17 @@ import connectDB from './lib/connectDB.js';
 import { clerkMiddleware, requireAuth } from '@clerk/express';
 import cors from 'cors';
 
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const app = express();
-app.use(cors(process.env.CLIENT_URL));
+app.use(cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true
+}));
+
 app.use(clerkMiddleware());
 app.use("/webhooks", webhookRouter);
 app.use(express.json());
@@ -43,15 +51,14 @@ app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
 
 app.use((error, req, res, next ) => {
-    res.status(error.status || 500);
-    res.json({
+    res.status(error.status || 500).json({
         message: error.message || "Something went wrong",
         status: error.status,
-        stack: error.stack,
     });
 })
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
     connectDB();
     console.log("Server is running");
 });
